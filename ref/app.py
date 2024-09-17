a8471ebffcdd4fbf5214d8dd33d1ca6b0c447f3d
Cris-UniGraz <cristian.hernandez@uni-graz.at>
2024-09-17 10:49:23 +0200

    Se descomentó parte del código en app.py

--- ref/app.py
+++ ref/app.py
@@ -31,7 +31,6 @@ def main(
     llm = (lambda x: azure_openai_call(x))  # Envolver la llamada en una función lambda
     docs = load_documents(folder_path=directory)
 
-    '''
     # print(f"\n\n>>> Documentos a procesar: {len(docs)}\n\n")
 
     embedding_model = load_embedding_model(model_name=EMBEDDING_MODEL_NAME)
@@ -99,7 +98,7 @@ def main(
                     break
         
         print("\n\n\n")
-    '''
+    
 
 if __name__ == "__main__":
 
