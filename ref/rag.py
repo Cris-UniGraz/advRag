26d13fd3b9cd7dcdbab5826b41cad8a5068f987e
Cris-UniGraz <cristian.hernandez@uni-graz.at>
2024-09-30 13:48:00 +0200

    Ajuste en los pesos del "ensemble_retriever".

--- ref/rag.py
+++ ref/rag.py
@@ -658,7 +658,7 @@ def get_ensemble_retriever_check(folder_path, embedding_model, llm, collection_n
             # weights=[0.0, 0.0, 1.0, 0.0, 0.0]
 
             # Pesos finales
-            weights=[0.2, 0.1, 0.3, 0.1, 0.3]
+            weights=[0.1, 0.1, 0.4, 0.1, 0.3]
         )
 
         return ensemble_retriever
